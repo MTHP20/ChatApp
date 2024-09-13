1979c8524820b5c001bc55ecef13d215305a09a9
Michael <mtpanlilio20@hotmail.com>
2024-09-13 22:50:32 +0100

    Made a server in Program.cs of ChatServer. If connected button is pressed then it writes to terminal that client is connected.

---
+++ ChatApp/MVVM/Core/RelayCommandBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Input;
+
+namespace ChatClient.MVVM.Core
+{
+    public class RelayCommandBase
+    {
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
