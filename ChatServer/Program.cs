fb09861d0dec9704ff84a533dc2f856812279d31
Michael <mtpanlilio20@hotmail.com>
2024-09-15 14:27:34 +0100

    Created broadcast function to show all clients connected.

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ChatServer.Net.IO;
+using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 
@@ -20,7 +22,23 @@ namespace ChatServer
                 _users.Add(client);
 
                 /*Broadcast connection to everyone on server*/
+                BroadcastConnction();
             }      
+        }
+
+        static void BroadcastConnction()
+        {
+            foreach (var user in _users)
+            {
+                foreach (var usr in _users)
+                {
+                    var broadcastPacket = new PacketBuilder();
+                    broadcastPacket.WriteOpCode(1);
+                    broadcastPacket.WriteMessage(usr.Username);
+                    broadcastPacket.WriteMessage(usr.UID.ToString());
+                    user.ClientSocket.Client.Send(broadcastPacket.GetPacketBytes());
+                }
+            }
         }
     }
 }
