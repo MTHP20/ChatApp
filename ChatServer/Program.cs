9483d89597305095fe1bacaa880c511b0b36b58a
Michael <mtpanlilio20@hotmail.com>
2024-09-15 13:41:41 +0100

    Create a new instance of Client

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -6,14 +6,17 @@ namespace ChatServer
 {
     class Program
     {
+        static List<Client> users;
         static TcpListener _listener;
         static void Main(String[] args)
         {
+            _users = new List<Client>();
             _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 7891);
             _listener.Start();
 
-            var client = _listener.AcceptTcpClient();
-            Console.WriteLine("Client has connected!");
+            var client = new Client(_listener.AcceptTcpClient());
+
+            
 
         }
     }
